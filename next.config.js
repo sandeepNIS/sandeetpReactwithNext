8ae5ea5a46cb42806dc25ef91aae9905393ca91d
sandeepNIS <sandeep.r@nichi.com>
2022-12-27 18:16:42 +0530

    modified some

--- next.config.js
+++ next.config.js
@@ -1,5 +1,27 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+function getBasePath() {
+  var basePath = ''
+
+  if (isProd && process.env.BASE_PATH){
+      if (process.env.BASE_PATH.startsWith("/") ){
+          basePath = process.env.BASE_PATH;
+      } else {
+          basePath = "/" + process.env.BASE_PATH;
+      }
+  } 
+
+  console.log("getBasePath() : isProd = " + isProd);
+  console.log("getBasePath() : basePath = " + basePath);
+
+  return basePath
+}
 const nextConfig = {
+  assetPrefix: getBasePath() ,
+
+    publicRuntimeConfig: {
+        basePath: getBasePath() ,
+    },
   reactStrictMode: true,
 }
 
