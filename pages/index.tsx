2074f8a49b46a29c436b542de1952014478d26a9
sandeepNIS <sandeep.r@nichi.com>
2023-01-02 12:13:07 +0530

    updated some changes in index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,24 +2,36 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Table } from "react-bootstrap";
 import AccountTableRow from "../components/AccountTableRow";
-const Listt = () => {
+
+export async function getStaticProps() {
+	// const allPostsData = getSortedPostsData();
+	const res = await fetch(`https://b6a4-119-82-104-94.in.ngrok.io/account/list`)
+	const posts = await res.json();
+	return {
+	  props: {
+		posts,
+	  },
+	};
+  }
+const Listt = ({posts}) => {
 const [accounts, setAccounts] = useState([]);
 
 
 	
-useEffect(() => {
-	axios
-	.get(`https://b6a4-119-82-104-94.in.ngrok.io/account/list`)
-	.then(({ data }) => {
-		setAccounts(data);
-	})
-	.catch((error) => {
-		console.log(error);
-	});
-}, []);
+// useEffect(() => {
+// 	axios
+// 	.get(`https://b6a4-119-82-104-94.in.ngrok.io/account/list`)
+// 	.then(({ data }) => {
+// 		setAccounts(data);
+// 	})
+// 	.catch((error) => {
+// 		console.log(error);
+// 	});
+// }, []);
+
 
 const DataTable = () => {
-	return accounts.map((res, i) => {
+	return posts.map((res, i) => {
 	return <AccountTableRow obj={res} key={i}   />;
 	});
 };
